a62d08e9418ea4aaa8a8d15c20a90e998e984c0e
Mustfalharis <111998046+Mustfalharis@users.noreply.github.com>
2023-12-18 11:05:41 +0000

    varsel

--- db/conf.js
+++ db/conf.js
@@ -1,12 +1,18 @@
 const { Client } = require('pg');
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'task_aon',
-    password: '12345',
-    port: 5432,
+const { Pool } = pg;
+const client = new Pool({
+connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
+
+
+// const client = new Client({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'task_aon',
+//     password: '12345',
+//     port: 5432,
+// })
 
 client.connect((err => console.log("connected")));
 
-module.exports = client;
+module.exports =client ;
